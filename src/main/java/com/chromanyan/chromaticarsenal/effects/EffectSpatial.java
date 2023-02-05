67f5a79a034640fc33520e25006fa0c393333e28
ChromaNyan <44331238+NyanMC@users.noreply.github.com>
2023-02-05 14:03:59 -0600

    final cleanup(?)

--- src/main/java/com/chromanyan/chromaticarsenal/effects/EffectSpatial.java
+++ src/main/java/com/chromanyan/chromaticarsenal/effects/EffectSpatial.java
@@ -2,10 +2,13 @@ package com.chromanyan.chromaticarsenal.effects;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraftforge.common.ForgeMod;
 
 public class EffectSpatial extends MobEffect {
 	public EffectSpatial() {
 		super(MobEffectCategory.BENEFICIAL, 0x000088);
+		this.addAttributeModifier(ForgeMod.ENTITY_GRAVITY.get(), "2ac7dff3-679e-48d7-8c04-f8989d118ff5", -0.25, AttributeModifier.Operation.MULTIPLY_TOTAL);
 	}
 
 }
