c256899beb26f485fc2f8c731a09303336b82bbb
ChromaNyan <nyancatz593@gmail.com>
2023-08-05 13:16:21 -0500

    make EffectSpatial reset fall distance every tick

--- src/main/java/com/chromanyan/chromaticarsenal/effects/EffectSpatial.java
+++ src/main/java/com/chromanyan/chromaticarsenal/effects/EffectSpatial.java
@@ -2,6 +2,7 @@ package com.chromanyan.chromaticarsenal.effects;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraftforge.common.ForgeMod;
 
@@ -11,4 +12,13 @@ public class EffectSpatial extends MobEffect {
         this.addAttributeModifier(ForgeMod.ENTITY_GRAVITY.get(), "2ac7dff3-679e-48d7-8c04-f8989d118ff5", -0.25, AttributeModifier.Operation.MULTIPLY_TOTAL);
     }
 
+    @Override
+    public void applyEffectTick(LivingEntity entity, int amplifier) {
+        entity.resetFallDistance();
+    }
+
+    @Override
+    public boolean isDurationEffectTick(int p_19455_, int p_19456_) {
+        return true;
+    }
 }
