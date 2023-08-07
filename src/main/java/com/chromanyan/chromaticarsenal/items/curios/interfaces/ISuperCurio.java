fb557af19e08f3c3561d276c8fa9f9d3ecb36379
ChromaNyan <nyancatz593@gmail.com>
2023-08-07 00:11:34 -0500

    add getInferiorVariant to ISuperCurio

--- src/main/java/com/chromanyan/chromaticarsenal/items/curios/interfaces/ISuperCurio.java
+++ src/main/java/com/chromanyan/chromaticarsenal/items/curios/interfaces/ISuperCurio.java
@@ -1,4 +1,8 @@
 package com.chromanyan.chromaticarsenal.items.curios.interfaces;
 
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.RegistryObject;
+
 public interface ISuperCurio {
+    RegistryObject<Item> getInferiorVariant();
 }
