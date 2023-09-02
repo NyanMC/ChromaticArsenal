9e3241e7229a7fbd4e5287984a52b0caae777400
ChromaNyan <nyancatz593@gmail.com>
2023-09-01 19:42:04 -0500

    lang for vertical stasis

--- src/main/java/com/chromanyan/chromaticarsenal/items/curios/CurioVerticalStasis.java
+++ src/main/java/com/chromanyan/chromaticarsenal/items/curios/CurioVerticalStasis.java
@@ -2,11 +2,24 @@ package com.chromanyan.chromaticarsenal.items.curios;
 
 import com.chromanyan.chromaticarsenal.items.base.BaseCurioItem;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import top.theillusivec4.curios.api.SlotContext;
 
+import java.util.List;
+
 public class CurioVerticalStasis extends BaseCurioItem {
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> list, @NotNull TooltipFlag flag) {
+        list.add(Component.translatable("tooltip.chromaticarsenal.vertical_stasis_stone.1"));
+        list.add(Component.translatable("tooltip.chromaticarsenal.vertical_stasis_stone.2"));
+    }
 
     @Override
     public void curioTick(SlotContext context, ItemStack stack) {
