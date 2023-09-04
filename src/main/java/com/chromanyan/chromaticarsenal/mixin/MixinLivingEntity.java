65fe5533a8515d2ac22424be83c3f7dcace407ed
ChromaNyan <nyancatz593@gmail.com>
2023-09-04 13:46:39 -0500

    fix shadow charm swift sneak applying multiple times

--- src/main/java/com/chromanyan/chromaticarsenal/mixin/MixinLivingEntity.java
+++ src/main/java/com/chromanyan/chromaticarsenal/mixin/MixinLivingEntity.java
@@ -8,25 +8,26 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantments;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
-import javax.annotation.Nullable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
 public class MixinLivingEntity {
-    private static final ModConfig.Common config = ModConfig.COMMON;
+    @Unique
+    private static final ModConfig.Common chromatic_workspace_19$config = ModConfig.COMMON;
 
-    @ModifyVariable(method = "getVisibilityPercent", at = @At("STORE"), ordinal = 0)
-    private double getVisibilityPercent(double d0, @Nullable Entity entity) {
+    @Inject(method = "getVisibilityPercent", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void getVisibilityPercent(Entity entity, CallbackInfoReturnable<Double> cir, double d0) {
         if (entity instanceof LivingEntity livingEntity) {
             if (ChromaCurioHelper.getCurio(livingEntity, ModItems.SHADOW_TREADS.get()).isPresent()) {
                 ItemStack stack = ChromaCurioHelper.getCurio(livingEntity, ModItems.SHADOW_TREADS.get()).get().stack();
-                if (stack.getEnchantmentLevel(Enchantments.SWIFT_SNEAK) > 0 && config.swiftSneakDetectionReduction.get() > 0) {
-                    return Math.max(0, d0 * (1 - (stack.getEnchantmentLevel(Enchantments.SWIFT_SNEAK) * config.swiftSneakDetectionReduction.get())));
+                if (stack.getEnchantmentLevel(Enchantments.SWIFT_SNEAK) > 0 && chromatic_workspace_19$config.swiftSneakDetectionReduction.get() > 0) {
+                    cir.setReturnValue(Math.max(0, d0 * (1 - (stack.getEnchantmentLevel(Enchantments.SWIFT_SNEAK) * chromatic_workspace_19$config.swiftSneakDetectionReduction.get()))));
                 }
             }
         }
-        return d0;
     }
 }
