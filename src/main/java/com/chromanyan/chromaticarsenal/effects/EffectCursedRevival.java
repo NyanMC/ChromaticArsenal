be08e513cdb5ceef5bb1ec164be1fb931298560f
ChromaNyan <nyancatz593@gmail.com>
2023-11-17 21:26:19 -0600

    cursed revival effect (currently does nothing)

---
+++ src/main/java/com/chromanyan/chromaticarsenal/effects/EffectCursedRevival.java
@@ -0,0 +1,10 @@
+package com.chromanyan.chromaticarsenal.effects;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class EffectCursedRevival extends MobEffect {
+    public EffectCursedRevival() {
+        super(MobEffectCategory.HARMFUL, 0xFF0000);
+    }
+}
--- src/main/java/com/chromanyan/chromaticarsenal/init/ModPotions.java
+++ src/main/java/com/chromanyan/chromaticarsenal/init/ModPotions.java
@@ -1,10 +1,7 @@
 package com.chromanyan.chromaticarsenal.init;
 
 import com.chromanyan.chromaticarsenal.ChromaticArsenal;
-import com.chromanyan.chromaticarsenal.effects.EffectBubblePanic;
-import com.chromanyan.chromaticarsenal.effects.EffectChilled;
-import com.chromanyan.chromaticarsenal.effects.EffectFractured;
-import com.chromanyan.chromaticarsenal.effects.EffectSpatial;
+import com.chromanyan.chromaticarsenal.effects.*;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,5 +14,6 @@ public class ModPotions {
     public static final RegistryObject<MobEffect> SPATIAL = EFFECTS_REGISTRY.register("spatial", EffectSpatial::new);
     public static final RegistryObject<MobEffect> CHILLED = EFFECTS_REGISTRY.register("chilled", EffectChilled::new);
     public static final RegistryObject<MobEffect> BUBBLE_PANIC = EFFECTS_REGISTRY.register("bubble_panic", EffectBubblePanic::new);
+    public static final RegistryObject<MobEffect> CURSED_REVIVAL = EFFECTS_REGISTRY.register("cursed_revival", EffectCursedRevival::new);
 
 }
