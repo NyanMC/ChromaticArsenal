11759929930b49fad31891d2c4d437515e1dfa23
ChromaNyan <nyancatz593@gmail.com>
2023-11-19 14:47:24 -0600

    remove ChromaBlock.java, register the chroma block as a RotatedPillarBlock

--- src/main/java/com/chromanyan/chromaticarsenal/init/ModBlocks.java
+++ src/main/java/com/chromanyan/chromaticarsenal/init/ModBlocks.java
@@ -1,15 +1,24 @@
 package com.chromanyan.chromaticarsenal.init;
 
 import com.chromanyan.chromaticarsenal.ChromaticArsenal;
-import com.chromanyan.chromaticarsenal.blocks.ChromaBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-@SuppressWarnings("all")
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS_REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, ChromaticArsenal.MODID);
 
-    public static final RegistryObject<Block> CHROMA_BLOCK = BLOCKS_REGISTRY.register("chroma_block", () -> new ChromaBlock());
+    public static final RegistryObject<Block> CHROMA_BLOCK = BLOCKS_REGISTRY.register("chroma_block",
+            () -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.DIAMOND)
+                    .requiresCorrectToolForDrops()
+                    .strength(5.0F, 6.0F)
+                    .sound(SoundType.METAL)
+                    .lightLevel((state) -> 15))
+    );
 }
