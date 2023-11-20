3a60f7d96cba52d820c003ef9759f203a7b5606c
ChromaNyan <nyancatz593@gmail.com>
2023-11-19 21:33:59 -0600

    blahaj tooltips and lang

--- src/main/java/com/chromanyan/chromaticarsenal/items/curios/utility/CurioBlahaj.java
+++ src/main/java/com/chromanyan/chromaticarsenal/items/curios/utility/CurioBlahaj.java
@@ -4,30 +4,41 @@ import com.chromanyan.chromaticarsenal.ChromaticArsenal;
 import com.chromanyan.chromaticarsenal.init.ModBlocks;
 import com.chromanyan.chromaticarsenal.init.ModRarities;
 import com.chromanyan.chromaticarsenal.items.curios.interfaces.IChromaCurio;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.Phantom;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
+import java.util.List;
+
 public class CurioBlahaj extends BlockItem implements IChromaCurio, ICurioItem {
+
     public CurioBlahaj() {
         super(ModBlocks.BLAHAJ.get(), new Item.Properties()
                 .tab(ChromaticArsenal.GROUP)
                 .stacksTo(1)
                 .rarity(Rarity.RARE)
                 .defaultDurability(0));
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> list, @NotNull TooltipFlag flag) {
+        list.add(Component.translatable("tooltip.chromaticarsenal.utility"));
+        list.add(Component.translatable("tooltip.chromaticarsenal.blahaj.1"));
+        list.add(Component.translatable("tooltip.chromaticarsenal.blahaj.2"));
+        list.add(Component.translatable("tooltip.chromaticarsenal.blahaj.3"));
     }
 
     @Override
