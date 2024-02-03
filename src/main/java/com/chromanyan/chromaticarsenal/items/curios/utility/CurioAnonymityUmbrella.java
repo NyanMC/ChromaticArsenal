03aec0456dd386f4891ea42c75ff4679a50878af
ChromaNyan <nyancatz593@gmail.com>
2024-02-02 21:24:27 -0600

    now properly a utility curio

--- src/main/java/com/chromanyan/chromaticarsenal/items/curios/utility/CurioAnonymityUmbrella.java
+++ src/main/java/com/chromanyan/chromaticarsenal/items/curios/utility/CurioAnonymityUmbrella.java
@@ -1,19 +1,23 @@
 package com.chromanyan.chromaticarsenal.items.curios.utility;
 
 import com.chromanyan.chromaticarsenal.ChromaticArsenal;
+import com.chromanyan.chromaticarsenal.init.ModRarities;
 import com.chromanyan.chromaticarsenal.items.base.BaseCurioItem;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.ForgeMod;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 
 import java.util.List;
 import java.util.UUID;
@@ -35,4 +39,14 @@ public class CurioAnonymityUmbrella extends BaseCurioItem {
         return atts;
     }
 
+    @NotNull
+    @Override
+    public ICurio.SoundInfo getEquipSound(SlotContext slotContext, ItemStack stack) {
+        return new ICurio.SoundInfo(SoundEvents.WOOL_PLACE, 0.5F, 1);
+    }
+
+    @Override
+    public @NotNull Rarity getRarity(@NotNull ItemStack stack) {
+        return ModRarities.UTILITY;
+    }
 }
