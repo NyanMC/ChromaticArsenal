fbe92331e063a541ac065eb31dbe1a8cb87ac000
ChromaNyan <nyancatz593@gmail.com>
2024-02-16 15:32:28 -0600

    unused import

--- src/main/java/com/chromanyan/chromaticarsenal/items/curios/CurioAdvancingHeart.java
+++ src/main/java/com/chromanyan/chromaticarsenal/items/curios/CurioAdvancingHeart.java
@@ -21,7 +21,6 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
 
 import java.util.List;
