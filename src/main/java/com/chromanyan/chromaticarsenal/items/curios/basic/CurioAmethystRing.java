20773a10082346e90d113b3a1954a6e24b042658
ChromaNyan <nyancatz593@gmail.com>
2024-02-17 14:02:41 -0600

    don't allow the amethyst ring to be chromatically twisted

--- src/main/java/com/chromanyan/chromaticarsenal/items/curios/basic/CurioAmethystRing.java
+++ src/main/java/com/chromanyan/chromaticarsenal/items/curios/basic/CurioAmethystRing.java
@@ -1,6 +1,7 @@
 package com.chromanyan.chromaticarsenal.items.curios.basic;
 
 import com.chromanyan.chromaticarsenal.ChromaticArsenal;
+import com.chromanyan.chromaticarsenal.init.ModEnchantments;
 import com.chromanyan.chromaticarsenal.items.base.BaseCurioItem;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
@@ -9,6 +10,7 @@ import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraftforge.common.ForgeMod;
 import org.jetbrains.annotations.NotNull;
 import top.theillusivec4.curios.api.SlotContext;
@@ -35,5 +37,10 @@ public class CurioAmethystRing extends BaseCurioItem {
     @Override
     public ICurio.SoundInfo getEquipSound(SlotContext slotContext, ItemStack stack) {
         return new ICurio.SoundInfo(SoundEvents.AMETHYST_BLOCK_PLACE, 0.5F, 1);
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+        return enchantment != ModEnchantments.CHROMATIC_TWISTING.get() && super.canApplyAtEnchantingTable(stack, enchantment);
     }
 }
