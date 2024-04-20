c48907f7e123a9bce8f1d1f6e008925acf17ac4c
ChromaNyan <nyancatz593@gmail.com>
2024-04-19 21:32:37 -0500

    feat: thunderguard allows channeling to always work

---
+++ src/main/java/com/chromanyan/chromaticarsenal/mixin/MixinThrownTrident.java
@@ -0,0 +1,32 @@
+package com.chromanyan.chromaticarsenal.mixin;
+
+import com.chromanyan.chromaticarsenal.init.ModItems;
+import com.chromanyan.chromaticarsenal.util.ChromaCurioHelper;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.ThrownTrident;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ThrownTrident.class)
+public class MixinThrownTrident {
+
+    @Inject(method = "<init>(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;)V", at = @At("RETURN"))
+    private void thrownTrident(Level level, LivingEntity livingEntity, ItemStack itemStack, CallbackInfo ci) {
+        if (EnchantmentHelper.hasChanneling(itemStack) && ChromaCurioHelper.getCurio(livingEntity, ModItems.THUNDERGUARD.get()).isPresent()) {
+            ((ThrownTrident)(Object) this).getPersistentData().putBoolean("chromaticarsenal.thunderguard_boost", true);
+        }
+    }
+
+    @Redirect(method = "onHitEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;isThundering()Z"))
+    private boolean shouldBypassThunderRequirement(Level instance) {
+        //noinspection UnreachableCode what is wrong with intellij
+        return ((ThrownTrident)(Object) this).getPersistentData().getBoolean("chromaticarsenal.thunderguard_boost") || instance.isThundering();
+    }
+
+}
