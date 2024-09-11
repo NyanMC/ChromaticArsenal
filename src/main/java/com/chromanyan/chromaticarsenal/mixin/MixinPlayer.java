64ffb7ac9a1b5a1c50c792dbd67adeaddea9efb8
ChromaNyan <nyancatz593@gmail.com>
2024-09-10 22:21:11 -0500

    refactor: unused import

--- src/main/java/com/chromanyan/chromaticarsenal/mixin/MixinPlayer.java
+++ src/main/java/com/chromanyan/chromaticarsenal/mixin/MixinPlayer.java
@@ -11,7 +11,6 @@ import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import top.theillusivec4.curios.api.SlotResult;
 
 import java.util.Optional;
