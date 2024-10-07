3e8ceec659d63e1fcae18105753b9fb64732b510
ChromaNyan <nyancatz593@gmail.com>
2024-10-06 23:38:20 -0500

    fix: annoying tooltip quirk

--- src/main/java/com/chromanyan/chromaticarsenal/items/curios/advanced/CurioPolychromaticFeather.java
+++ src/main/java/com/chromanyan/chromaticarsenal/items/curios/advanced/CurioPolychromaticFeather.java
@@ -38,8 +38,8 @@ public class CurioPolychromaticFeather extends BaseSuperCurio {
     @OnlyIn(Dist.CLIENT)
     public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> list, @NotNull TooltipFlag flag) {
         super.appendHoverText(stack, level, list, flag);
-        if (!Screen.hasShiftDown()) return;
         list.add(Component.translatable("tooltip.chromaticarsenal.utility"));
+        if (!Screen.hasShiftDown()) return;
         list.add(Component.translatable("tooltip.chromaticarsenal.super_harpy_feather.1"));
         list.add(Component.translatable("tooltip.chromaticarsenal.super_harpy_feather.2"));
     }
